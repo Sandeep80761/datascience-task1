d7a15448406cce473052e6a9d6a2b6f95d09dbc4
Sandeep80761 <138312519+Sandeep80761@users.noreply.github.com>
2024-07-11 00:43:49 +0530

    Add files via upload

---
+++ task1 .py
@@ -0,0 +1,43 @@
+# list 
+my_list = [1,2,3]
+print("list",my_list)
+
+# addition of element
+my_list.append(4)
+print("after addition",my_list)
+
+# removal of element
+my_list.remove(1)
+print("after removal",my_list)
+
+# modification of element
+my_list[2]=5
+print("after modification",my_list)
+
+# dictionary
+my_dict = {'a': 1, 'b': 2,'c': 3}
+print("dictionary",my_dict)
+
+# addition
+my_dict ['d'] = 4
+print("after addition",my_dict)
+
+# removal of value
+del my_dict['c']
+print("after removal",my_dict)
+
+# modification of value
+my_dict ['c'] = 7
+print("after modification",my_dict)
+
+# set
+my_set = {1,2,3}
+print("set",my_set)
+
+# addition
+my_set.add(4)
+print("after addition",my_set)
+
+# removal
+my_set.remove(1)
+print("after removal",my_dict)
